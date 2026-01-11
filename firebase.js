f5a46030b6e3f02a983be45595f27f97103e5ecb
Anmol Bhadade <131282176+ABpatilhacker@users.noreply.github.com>
2026-01-12 01:25:51 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,10 @@
-const firebaseConfig = {
+<!-- Firebase SDKs -->
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+  const firebaseConfig = {
   apiKey: "AIzaSyB3ytMC77uaEwdqmXgr1t-PN0z3qV_Dxi8",
   authDomain: "smart-attendance-system-17e89.firebaseapp.com",
   databaseURL: "https://smart-attendance-system-17e89-default-rtdb.firebaseio.com",
@@ -7,6 +13,9 @@ const firebaseConfig = {
   messagingSenderId: "168700970246",
   appId: "1:168700970246:web:392156387db81e92544a87"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const database = firebase.database();
+
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+  const database = firebase.database();
+</script>
