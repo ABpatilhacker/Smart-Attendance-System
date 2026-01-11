16374a1275f1edb5d3610ee94d144a0d5aa2e2d5
Anmol Bhadade <131282176+ABpatilhacker@users.noreply.github.com>
2026-01-11 23:16:53 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -16,3 +16,11 @@ function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("open");
 }
+document.addEventListener("click", (e) => {
+  const sidebar = document.getElementById("sidebar");
+  const btn = document.querySelector(".menu-btn");
+
+  if (!sidebar.contains(e.target) && !btn.contains(e.target)) {
+    sidebar.classList.remove("open");
+  }
+});
