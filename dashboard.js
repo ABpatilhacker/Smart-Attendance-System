b4b4a833367bce42da59daae5c4e5b0c98f97e54
Anmol Bhadade <131282176+ABpatilhacker@users.noreply.github.com>
2026-01-12 01:16:35 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,17 @@
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
-
-  if (!sidebar) {
-    alert("Sidebar not found ❌");
-    return;
-  }
-
   sidebar.classList.toggle("open");
+}
+
+function showSection(sectionId) {
+  // hide all sections
+  document.querySelectorAll(".section").forEach(sec => {
+    sec.style.display = "none";
+  });
+
+  // show selected section
+  document.getElementById(sectionId).style.display = "block";
+
+  // close sidebar after click (MOBILE FIX ✅)
+  document.getElementById("sidebar").classList.remove("open");
 }
