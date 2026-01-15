86057343bd3627060401154b135e6ca26a353acc
Anmol Bhadade <131282176+ABpatilhacker@users.noreply.github.com>
2026-01-15 18:17:30 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
-// firebase.js
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB3ytMC77uaEwdqmXgr1t-PN0z3qV_Dxi8",
   authDomain: "smart-attendance-system-17e89.firebaseapp.com",
   databaseURL: "https://smart-attendance-system-17e89-default-rtdb.firebaseio.com",
   projectId: "smart-attendance-system-17e89",
-  storageBucket: "smart-attendance-system-17e89.appspot.com",
+  storageBucket: "smart-attendance-system-17e89.firebasestorage.app",
   messagingSenderId: "168700970246",
   appId: "1:168700970246:web:392156387db81e92544a87"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Make global (IMPORTANT)
-window.auth = firebase.auth();
-window.db = firebase.database();
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+</script>
